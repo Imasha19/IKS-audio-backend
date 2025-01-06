bd99e40cd15f95ab6f8ae13bcbf96d96fbe7bab7
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-01-06 23:25:20 +0530

    created backend for reviews☑️

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import reviewRouter from "./routes/reviewRouter.js";
 
 dotenv.config();
 
@@ -42,12 +43,19 @@ connection.once("open",()=>{
     console.log("MongoDB connection established successfully")
 })
 
-app.use("/api/users",userRouter)
-app.use("/api/products",productRouter)
+app.use("/api/users",userRouter);
+app.use("/api/products",productRouter);
+app.use("/api/reviews",reviewRouter);
+
 
 app.listen(3000,()=>{
    console.log("Server is running on port 3000")
 
 
-})
+});
 
+//customer-"email": "bobjohnson@example.com",
+//   "password": "securehashedpassword456"
+
+//admin-"email": "johndoeKam@example.com",
+//    "password": "securepassword123",
