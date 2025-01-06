c6e8dea1a1c63dbfb1d9ac7d456d5a71a3ad30c5
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-01-07 00:33:26 +0530

    💡 implemented reviewController as only admin can see all the reviews

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { addReview } from "../controller/reviewController.js";
+import { addReview, getReviews } from "../controller/reviewController.js";
 
 const reviewRouter =express.Router();
 reviewRouter.post("/",addReview)
+reviewRouter.get("/",getReviews)
 
 export default reviewRouter;
