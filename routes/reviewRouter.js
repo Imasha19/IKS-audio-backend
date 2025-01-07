2b1457642c0713ce95f31b7854bc03b243621fe9
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-01-08 01:41:34 +0530

    implemented reviews with deleteReview function🔍

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addReview, getReviews } from "../controller/reviewController.js";
+import { addReview, deleteReview, getReviews } from "../controller/reviewController.js";
 
 const reviewRouter =express.Router();
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReviews)
+reviewRouter.delete("/:email",deleteReview)
 
 export default reviewRouter;
